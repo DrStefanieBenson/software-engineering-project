967e9f4e4fb869c5f4fe2643f34c50331ad3a270
DrStefanieBenson <124464480+DrStefanieBenson@users.noreply.github.com>
2023-06-11 12:38:04 -0400

    app.py

--- app.py
+++ app.py
@@ -4,19 +4,34 @@ import plotly.express as px
 
 df_car_ads = pd.read_csv('vehicles_us.csv')
 
+df_car_ads_mod = pd.DataFrame(df_car_ads.loc[df_car_ads["model_year"] > 2000])
+df_car_ads_mod= pd.DataFrame(df_car_ads_mod.loc[df_car_ads_mod['price'] < 42498])
+
 st.header('The Number of Days Ad is Listed Per Price and Vehicle Condition')
-fig = px.scatter(df_car_ads, x="days_listed", y="price", color="condition",
+fig = px.scatter(df_car_ads_mod, x="days_listed", y="price", color="condition",
                  size='days_listed', hover_data=['make','type'], color_discrete_sequence=px.colors.qualitative.Light24,
                  width = 1000, height = 1000, title='The Number of Days Ad is Listed Per Price and Condition of Vehicle')
 fig.show()
 
 
 st.header('Number of Ads Per Car Manufacturer')
-ads_by_make= pd.DataFrame(df_car_ads.groupby('make')['model'].count())
+ads_by_make= pd.DataFrame(df_car_ads_mod.groupby('make')['model'].count())
 ads_by_make.reset_index(inplace=True)
 ads_by_make.columns = ['make', 'number of ads']
+fig = px.pie(ads_by_make,
+             values='number of ads',
+             names='make',
+             title='Number of Ads Per Car Manufacturer',
+             color='make',
+             color_discrete_sequence=px.colors.qualitative.Light24)
+
+fig.show()
+
 
 st.header('Number of Ads Per Vehicle Type')
+ads_by_type= pd.DataFrame(df_car_ads_mod.groupby('type')['make'].count())
+ads_by_type.reset_index(inplace=True)
+ads_by_type.columns = ['type', 'number_of_ads']
 fig = px.bar(ads_by_type,
              x = 'type',
              y = 'number_of_ads',
